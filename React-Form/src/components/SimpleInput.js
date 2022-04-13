73f812fcc310244bb55e6b6ae514c1f366d32cec
AnjasmaraDS <88811992+AnjasmaraDS@users.noreply.github.com>
2022-04-13 14:34:14 +0700

    try use form input basic

--- React-Form/src/components/SimpleInput.js
+++ React-Form/src/components/SimpleInput.js
@@ -1,10 +1,64 @@
+import {useRef, useState, useEffect} from "react";
+import {getCLS} from "web-vitals";
+
 const SimpleInput = (props) => {
+    const usingRef = useRef()
+    const [getEnteredName, setEnteredName] = useState('')
+    const [getValidation, setValidation] = useState(false)
+    const [getTouchableInput, setTouchableInput] = useState(false)
+    const [getNameBlurHandler, setNameBlurHandler] = useState(false)
+
+    const nameInputHandler = (event) =>{
+        setEnteredName(event.target.value)
+        if(event.target.value.trim() === ''){
+            setValidation(true)
+        }
+    }
+
+    useEffect(()=>{
+        if(!getValidation){
+            console.log('Name input is valid');
+        }
+    }, [getValidation])
+
+    const formSubmissionHandler = (event) =>{
+        event.preventDefault()
+        setTouchableInput(true)
+        if(getEnteredName.trim() === ''){
+            setValidation(true)
+            return
+        }
+        setValidation(false)
+        console.log('menggunakan state');
+        console.log(getEnteredName)
+        // const refObjeck= usingRef.current.value
+        // console.log('menggunakan ref');
+        // console.log(refObjeck);
+
+        //nameInputRef.current.value = ''; =>Not Ideal, Don't Manipulate The DOM
+        setEnteredName('');
+    }
+    const nameInputBlur = (event)=>{
+        setNameBlurHandler(true);
+        if(getEnteredName.trim() === ''){
+            setValidation(true);
+            return;
+        }
+    }
+    const nameInputIsInvalid = getValidation && getTouchableInput
+    const nameInputClass = !nameInputIsInvalid ? 'form-control' : 'form-control invalid';
   return (
-    <form>
-      <div className='form-control'>
+    <form onSubmit={formSubmissionHandler}>
+      <div className={nameInputClass}>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+        <input
+            ref = {usingRef}
+            type='text'
+            id='name'
+            onBlur ={nameInputBlur}
+            onChange={nameInputHandler} />
       </div>
+        {nameInputIsInvalid && <p className={'error-text'}>masukan huruf di input ini </p>}
       <div className="form-actions">
         <button>Submit</button>
       </div>
